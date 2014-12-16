5fb353bcdad045bc13bc8e68ee27cb62ee46dc20
liceu <enguillem@gmail.com>
2014-12-16 10:41:42 +0100

    llista clients

---
+++ php/delete_email.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>untitled</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.23.1" />
+</head>
+
+<body>
+    <?php
+    $dbc = mysqli_connect('172.30.10.172', 'guillem', 'guillem', 'elvis_store')
+        or die('Error connecting to MySQL server.');
+        
+    $sql="SELECT * FROM email_list";
+    $result = mysqli_query($dbc, $sql)
+    or die('Error querying database.');
+    
+    ?>
+    
+    <h1>llistat clients</h1>
+    
+    
+    <?php
+        
+    while($row = mysqli_fetch_array($result)){
+            echo $row['id']." ".$row['first_name']." ".$row['last_name']." ".$row['email']."<br>";
+        
+    }
+     
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    ?>
+	
+</body>
+
+</html>
