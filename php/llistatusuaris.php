c6007fe2fa48aaff09ddeaa6fe436ee0a129228f
liceu <enguillem@gmail.com>
2015-01-13 10:18:38 +0100

    exemple esborrant usuari amb checkbox

--- php/llistatusuaris.php
+++ php/llistatusuaris.php
@@ -12,8 +12,21 @@
     <?php
     $dbc = mysqli_connect('172.16.82.211', 'guillem', 'guillem', 'sirius_db')
         or die('Error connecting to MySQL server.');
+        
+   if (isset($_POST['submit'])){
+       echo "esborrar usuaris<br>";
+       foreach ($_POST['todelete'] as $delete_id){
+           echo "...esborrant $delete_id<br>";
+           $sql="DELETE FROM users where id=$delete_id";
+           $result = mysqli_query($dbc, $sql) or die('Error querying database.');
+       }
+       
+    }     
+        
     $sql="select id,usuari,nom,llinatge1,llinatge2 from users";
     $result = mysqli_query($dbc, $sql) or die('Error querying database.');
+    
+    
     ?>
     <table border="1">
         <tr>
@@ -23,11 +36,13 @@
             <th>Llinatge1</th>
             <th>Llinatge2</th>
         </tr>
+        
+    <form method="post" action="llistatusuaris.php">    
     <?php
     while($row = mysqli_fetch_array($result)){
         ?>
         <tr>
-            <td>&nbsp;</td>
+            <td><input type="checkbox" value="<?= $row['id']?>" name="todelete[]" /></td>
             <td><a href="updateuser.php?id=<?= $row['id']?>"><?=$row['usuari']?></a></td>
             <td><?=$row['nom']?></td>
             <td><?=$row['llinatge1']?></td>
@@ -38,6 +53,8 @@
         }
     ?>
 	</table>
+    <input type="submit" name="submit" value="esborrar seleccionats" />
+    </form>
 </body>
 
 </html>
