f6cde0175e161389beba5be7acd61b5beaa44dc3
liceu <enguillem@gmail.com>
2015-02-19 09:48:42 +0100

    el dni ja valida i he introduit el camp de data

--- JAVA_SCRIPT/formulari.php
+++ JAVA_SCRIPT/formulari.php
@@ -11,18 +11,36 @@
         function dniValid(valor){
             //retornar vertader o fals en funcio de si el dni és correcte
             //trobareu el codi al punt 7.3 de la documentació
+            var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+             
+            if( !(/^\d{8}[A-Z]$/.test(valor)) ) {
+              return false;
+            }
+             
+            if(valor.charAt(8) != letras[(valor.substring(0, 8))%23]) {
+              return false;
+            }
             
+            return true;
             
+        }
+        
+        function dataValid(valor){
+                //si valor és una data correcta retornam true, sino false
+                
         }
         function valida(){
                 var form=document.getElementById("formulari");
                 var nom=document.getElementById("nom");
                 var dni=document.getElementById("dni");
+                var dataNac=document.getElementById("dataNac");
                 var form_validat=false;
                 if(nom.value==""){
                         alert("has d'introduir un nom");
                 }else if(!dniValid(dni.value)){
                         alert("has d'introduir un dni correcte");
+                }else if(!dataValid(dataNac.value)){
+                    alert("La data de naixament no és correcte");
                 }else{
                     form_validat=true;
                 }
@@ -36,6 +54,8 @@
     <form action="formulari.php" id="formulari">
         <label>Nom:</label><input type="text" value="" id="nom" name="nom"/><br/>
         <label>Dni:</label><input type="text" value="" id="dni" name="dni"/><br/>
+        <label>Data naixament:</label>
+        <input type="text" value="" id="dataNac" name="dataNac"/><br/>
         <input type="button" onclick="valida()" value="desa"/>
     </form>
 </body>
