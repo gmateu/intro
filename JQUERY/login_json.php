ef98e49c1cce22aa0f679498ef69e2d943120e32
liceu <enguillem@gmail.com>
2015-03-09 12:17:01 +0100

    exemple login per JQUERY

--- JQUERY/login_json.php
+++ JQUERY/login_json.php
@@ -3,13 +3,18 @@
         or die('Error connecting to MySQL server.');
     $usuari=$_GET['usuari'];
     $password=$_GET['password'];
-    $sql="SELECT password FROM sirius_db.users where usuari='$usuari'";
-    $result = mysqli_query($dbc, $sql) or die('Error querying database.');
+    $sql="SELECT password,nom,llinatge1,llinatge2 FROM 
+    sirius_db.users where usuari='$usuari'";
+    $result = mysqli_query($dbc, $sql) 
+    or die('Error querying database.');
     $row = mysqli_fetch_array($result);
     $rows=array();
     
     if ($password==$row['password']){
             $rows[]="s";
+            $rows[]=$row['nom'];
+            $rows[]=$row['llinatge1'];
+            $rows[]=$row['llinatge2'];
     }else{
             $rows[]="n";
     }
